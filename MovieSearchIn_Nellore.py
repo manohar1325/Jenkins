70736bd4a6ced2b518e704a8215ce8ae6ff9c068
manohar1325 <manoharmanu1325@gmail.com>
2023-11-10 16:54:48 +0530

    adding one python file

---
+++ MovieSearchIn_Nellore.py
@@ -0,0 +1,20 @@
+# import time
+#
+# from selenium import webdriver
+# from selenium.webdriver import ActionChains
+# from selenium.webdriver.common.by import By
+# from webdriver_manager.chrome import ChromeDriverManager
+#
+# driver = webdriver.Chrome()
+# driver.maximize_window()
+# driver.get("https://in.bookmyshow.com/explore/movies-nellore")
+#
+# movie_names = driver.find_elements(By.XPATH, "//div[@class = 'style__StyledText-sc-7o7nez-0 dxpBCo']")
+#
+# for index, movie in enumerate(movie_names):
+#     print(f"{index + 1}.{movie.text}")
+
+
+a = 8
+b = 10
+print(b)
